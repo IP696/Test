37844cd6d35906a0db00206fe7c430254b94fb17
ppetrashov <ppetrashov@healthgorilla.com>
2020-06-22 20:06:20 +0300

    stuped comment

--- moduleOne/src/main/java/pavel/UserService.java
+++ moduleOne/src/main/java/pavel/UserService.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class UserService {
     private String hz;
 
+    /**
+     * Get systems statuses if down connections skip intervals is configured.
+     * Must be non-empty.
+     * @return
+     */
     public List getUsers() {
         List<User> users = new ArrayList<User>();
         users.add(new User());
